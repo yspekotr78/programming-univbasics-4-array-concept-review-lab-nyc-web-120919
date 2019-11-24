80acd1d972bfaa76c9a3d7f00c43ee410031ccef
Yury Spektor <yspektor78@gmail.com>
2019-11-24 22:32:27 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
    array.length.times { |index|
    if array[index]=value_to_find
      return index
-   else return nil
+   else return index
  }
   
 end
