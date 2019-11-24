2828d52e6741f1222dd46edc12073d70829042b8
Yury Spektor <yspektor78@gmail.com>
2019-11-24 22:52:09 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,8 +3,8 @@ def find_element_index(array, value_to_find)
      if array[index]==value_to_find
        return index
      end
-   return nil
-  end
+   end
+  return nil
 end
 
 def find_max_value(array)
